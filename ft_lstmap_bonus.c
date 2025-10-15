0662c961d78c0145eb2279b44cdfd75ce3bab35f
Sergio Alejandro <sergioromero2k@gmail.com>
2025-10-15 20:35:02 +0200

    Acabando libft 42

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: sergio-alejandro <sergio-alejandro@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 17:44:27 by sergio-alej       #+#    #+#             */
-/*   Updated: 2025/10/15 06:59:26 by sergio-alej      ###   ########.fr       */
+/*   Updated: 2025/10/15 20:32:21 by sergio-alej      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,21 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*lst_new;
+	t_list	*tmp;
 
-	if (!lst || !f || !del)
+	lst_new = NULL;
+	if (!lst)
 		return (NULL);
-	lst_new = lst;
-	while (lst_new)
+	while (lst)
 	{
-		while (!lst_new->content)
+		tmp = ft_lstnew(f(lst->content));
+		ft_lstadd_back(&lst_new, tmp);
+		if (!tmp)
 		{
-			del(lst_new->content);
-			lst_new = lst_new->next;
+			ft_lstclear(&lst_new, del);
+			return (NULL);
 		}
-		if (!lst_new->content)
-			return (lst_new);
-		f(lst_new->content);
-		lst_new = lst_new->next;
+		lst = lst->next;
 	}
 	return (lst_new);
 }
